f35869823d701c5586b8b86de615e8d1bd8d365e
rath-prak <rathanap@gmail.com>
2016-06-08 20:13:06 +1200

    set up landing page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 // Note: See http://blog.garstasio.com/you-dont-need-jquery/ and http://youmightnotneedjquery.com/ for JS commands that don't require jQuery
 $(document).ready(function(){
+
+// initialize pagepilling
+$('#pagepiling').pagepiling({direction: 'vertical', loopBottom: 'true'});
+
+
 });
