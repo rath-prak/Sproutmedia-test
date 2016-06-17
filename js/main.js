4c08fbb189f16e1c3e50ef550936c911424b7dce
rath-prak <rathanap@gmail.com>
2016-06-17 13:01:38 +1200

    change scrolling for landing page

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 // Note: See http://blog.garstasio.com/you-dont-need-jquery/ and http://youmightnotneedjquery.com/ for JS commands that don't require jQuery
 $(document).ready(function(){
 
-// initialize pagepilling
-$('#pagepiling').pagepiling({direction: 'vertical', loopBottom: 'true'});
+// // initialize pagepilling
+// $('#pagepiling').pagepiling({
+// 	direction: 'vertical', 
+// 	loopBottom: 'true',
+// 	anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+//     menu: '#myMenu'
+// 	});
+
+// initialize fullpage
+
+    $('#fullpage').fullpage({
+    	//Navigation
+        menu: '#main-menu',
+        lockAnchors: false,
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        showActiveTooltip: false,
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+    });
+
 
 
 });
